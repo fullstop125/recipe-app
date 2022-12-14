bbd7f13add7429e1f1b8e319802804ea344200bb
Profsain <profsaincrown@gmail.com>
2022-12-14 01:22:05 +0000

    Add validations and association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ApplicationRecord
+  # Associations
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
+
+  # validations
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :public, inclusion: { in: [true, false] }
+  validates :user_id, presence: true
 end
