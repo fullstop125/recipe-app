7ef482112517b883ac12f80bed7e4c17f7300c86
Profsain <profsaincrown@gmail.com>
2022-12-14 01:14:37 +0000

    add user association and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Associations
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+  
+  # validations
+  validates :name, presence: true, length: { maximum: 50 }
 end
