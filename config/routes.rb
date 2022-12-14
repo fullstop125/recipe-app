f0c308a536beebc57d304b8a3a88e63c92d5e563
Profsain <profsaincrown@gmail.com>
2022-12-14 11:24:55 +0000

    add recipes list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Defines routes for recipes
+  resources :recipes, only: [:index,]
 end
