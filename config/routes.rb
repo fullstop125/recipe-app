4d7f57a5fc18d5f4d36db5937de9df626c80c4b3
fullstop125 <hassanmoses125@gmail.com>
2022-12-16 00:20:49 +0300

    add authentication and add food list functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'recipes/index'
   devise_for :users
+  root 'foods#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
