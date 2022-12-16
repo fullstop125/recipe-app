a5b99c073aa4bbd8833ed8aaa0a917b39ed624eb
Profsain <profsaincrown@gmail.com>
2022-12-16 15:30:55 +0000

    render ingredients table

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,7 @@ class RecipesController < ApplicationController
   # GET /recipes/1 or /recipes/1.json
   def show
     @recipe = Recipe.find(params[:id])
+    @foods = Food.all
   end
 
   # GET /recipes/new or /recipes/new.json
