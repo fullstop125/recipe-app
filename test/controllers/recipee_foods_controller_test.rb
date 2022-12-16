b26dcbad9c4ef495a3c2dfd2efc0fac290a81811
fullstop125 <hassanmoses125@gmail.com>
2022-12-17 00:01:34 +0300

    add shopping list functionality and logout functionality

--- test/controllers/recipee_foods_controller_test.rb
+++ test/controllers/recipee_foods_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class RecipeeFoodsControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
+  test 'should get edit' do
     get recipee_foods_edit_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get recipee_foods_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get recipee_foods_create_url
     assert_response :success
   end
